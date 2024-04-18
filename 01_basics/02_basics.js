99aec19558f2a6ff312c9e86bc61b70c627bf6e0
Mihir Jain <129359677+imihirjain@users.noreply.github.com>
2024-04-18 17:50:33 +0000

    datatype

---
+++ 01_basics/02_basics.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3)
+console.log("Mihir Jain");
+
+let name = "Mihir"
+let age = 18
+let isLoggedIn = false
+
+console.log([name,age, isLoggedIn])
+
+/*
+    Some datatypes
+    number => 2 to power 53
+    biginit
+    string
+    boolean
+    null 
+    undefined
+
+    null type is object
+
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
