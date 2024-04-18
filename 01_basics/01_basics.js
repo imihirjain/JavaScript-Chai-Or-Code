07019edd2854b899fa0ccd9e2abf031cd94831f5
Mihir Jain <129359677+imihirjain@users.noreply.github.com>
2024-04-18 17:39:00 +0000

    basic

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,15 @@
+const accountId = 990586426523  
+let accountEmail = "manav@gmail.com"
+var accountPassword = "6531"
+accountCity = "Delhi"
+
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+
+// accountId = 1234567890 this is not allowed
+// console.log(accountId);
+
+accountEmail = "prachi@gmail.com"
+accountPassword = "754544"
+accountCity = "Aligarh"
+
+console.log([accountId, accountEmail, accountCity, accountPassword]);
