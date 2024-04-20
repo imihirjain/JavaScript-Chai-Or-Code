37df2767d344f44bfb31e4bc8f89f6041ff7320a
Mihir Jain <129359677+imihirjain@users.noreply.github.com>
2024-04-20 19:27:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionOperator.js
@@ -0,0 +1,39 @@
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = "33";
+// let score = "33abx";
+// let score = null;
+// let score = undefined;
+// let score = false;
+// let score = "Mihir";
+// let value = Number(score)
+
+// console.log(typeof value);
+// console.log(value);
+
+/* "33" => 33
+"33abc" => NaN
+true => 1 or false => 0
+*/
+
+// let isLoggedIn = "";
+// let isLoggedIn = "Mihir";
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+
+// console.log(typeof (booleanIsLoogedIn));
+// console.log(booleanIsLoogedIn);
+
+/*
+1 => true and 0 => false
+"" => false
+"Mihir" => true;
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(typeof(stringNumber));
+console.log(stringNumber);
